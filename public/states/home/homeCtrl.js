f2e5bf6accbb5fe8bd0a522d76f3dc25e891c05f
Erik Golden <erikgolden7@gmail.com>
2017-06-06 16:55:08 -0600

    added increment functionality

--- public/states/home/homeCtrl.js
+++ public/states/home/homeCtrl.js
@@ -1,23 +1,56 @@
 
 angular.module('qaApp').controller('homeCtrl', function($scope) {
 	
-	$scope.count = 7;
-	$scope.totalCount = 59302;
+	// $scope.todayCount = 0;
+	$scope.totalCount = 0;
 	
-	// let getCount = () => {
-	// 	homeService.getCount().then(function(res){
-	// 		$scope.count = res.data;
+	// let getTodayCount = () => {
+	// 	homeService.getTodayCount().then(function(res){
+	// 		$scope.todayCount = res.data;
 	// 	});
 	// };
-	// getCount();
+	// getTodayCount();
+	//
+	// let getTotalCount = () => {
+	// 	homeService.getTotalCount().then(function(res){
+	// 		$scope.totalCount = res.data;
+	// 	});
+	// };
+	// getTotalCount();
 	
 	
+	$scope.todayCount = 0;
+	let max = $scope.todayCount + 99999;
+	let min = $scope.todayCount;
 	
-	$scope.increment = () => {
-		let date = new Date();
-		homeService.increment(count, date);
+	$scope.increment = function() {
+		if ($scope.todayCount >= max) { return; }
+		$scope.todayCount++;
+		$scope.totalCount++;
+	};
+	$scope.decrement = function() {
+		if ($scope.todayCount <= min) { return; }
+		$scope.todayCount--;
+		$scope.totalCount--;
 	};
 	
+	
+	
+	
+	// $scope.increment = () => {
+	// 	// let date = new Date();
+	// 	// homeService.increment(count, date);
+	// 	todayCount++;
+	// 	console.log(todayCount);
+	// 	return todayCount;
+	// };
+	//
+	// $scope.decrement = (todayCount) => {
+	// 	// let date = new Date();
+	// 	// homeService.decrement(count, date);
+	// 	todayCount--;
+	// 	return todayCount;
+	// };
 	
 	
 });
--- public/states/home/homeService.js
+++ public/states/home/homeService.js
@@ -1,6 +1,31 @@
 angular.module('qaApp').service('homeService', function($http, $q) {
 	
-	this.updateCounter = (count, date) => {
+	this.getTodayCount = function() {
+		return $http ({
+			method: 'GET',
+			url: '/getTodayCount'
+		})
+	};
+	
+	this.getTotalCount = function() {
+		return $http ({
+			method: 'GET',
+			url: '/getTotalCount'
+		})
+	};
+	
+	this.increment = (count, date) => {
+		return $http({
+			method: 'PUT',
+			url: '/updateCounter',
+			data: {
+				'count' : count,
+				'date' : date
+			}
+		})
+	};
+	
+	this.decrement = (count, date) => {
 		return $http({
 			method: 'PUT',
 			url: '/updateCounter',
