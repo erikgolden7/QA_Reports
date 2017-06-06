c88152489200981451029d711dea0f324d7e7a1e
Erik Golden <erikgolden7@gmail.com>
2017-06-06 15:57:11 -0600

    added logo and changed increment/decrement buttons

--- public/states/home/homeCtrl.js
+++ public/states/home/homeCtrl.js
@@ -2,23 +2,22 @@
 angular.module('qaApp').controller('homeCtrl', function($scope) {
 	
 	$scope.count = 7;
+	$scope.totalCount = 59302;
+	
+	// let getCount = () => {
+	// 	homeService.getCount().then(function(res){
+	// 		$scope.count = res.data;
+	// 	});
+	// };
+	// getCount();
+	
+	
 	
 	$scope.increment = () => {
 		let date = new Date();
-		
+		homeService.increment(count, date);
 	};
 	
 	
-	
-	
-	
-	
-	
-	
-	$scope.htmlEndTime = () => {
-		let date = new Date();
-		assessmentService.htmlEndTime($scope.user.id, date);
-		$scope.stop1 = false;
-	};
 	
 });
--- public/states/home/homeService.js
+++ public/states/home/homeService.js
@@ -1,4 +1,14 @@
 angular.module('qaApp').service('homeService', function($http, $q) {
-
+	
+	this.updateCounter = (count, date) => {
+		return $http({
+			method: 'PUT',
+			url: '/updateCounter',
+			data: {
+				'count' : count,
+				'date' : date
+			}
+		})
+	};
 
 });
