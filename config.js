bcdfa49f7e65325b87b51f6be21a6b0bbc9f58c8
Erik Golden <erikgolden7@gmail.com>
2017-06-09 12:49:02 -0600

    changed to elephant sql db

--- config.js
+++ config.js
@@ -1,16 +1,7 @@
-
-// module.exports = {
-// 	port:3000,
-// 	postgresUser:'postgres',
-// 	postgresPass:'whocaresaboutsecurity'
-// };
-
-
 module.exports = {
 	port:3000,
 	postgresUser:'ypljdmop',
 	postgresPass:'1GgJ9SPiNfxxPnnLcQdzJ7Ae8TjuixRe'
 };
-
 
 
