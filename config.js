99f07c7608e218d4efa8b57ca5468fb2336a9146
Erik Golden <erikgolden7@gmail.com>
2017-06-09 18:53:11 +0000

    config

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-
 module.exports = {
-	port:3000,
-	postgresUser:'postgres',
-	postgresPass:''
+	port:8083,
+	postgresUser:'ypljdmop',
+	postgresPass:'1GgJ9SPiNfxxPnnLcQdzJ7Ae8TjuixRe'
 };
--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,11 +1,11 @@
 angular.module('qaApp', ['ui.router']).config(function($stateProvider, $urlRouterProvider) {
 	
-	$urlRouterProvider.otherwise('/login');
+	$urlRouterProvider.otherwise('/');
 	
 	$stateProvider
 	.state('home', {
-		url: '/home',
-		templateUrl: './index.html',
+		url: '/',
+		templateUrl: '../states/home/home.html',
 		controller: 'homeCtrl'
 	})
 	.state('day', {
@@ -41,6 +41,32 @@ angular.module('qaApp').controller('allCtrl', function($scope) {
 	// 	$scope.stop1 = false;
 	// };
 	
+	var ctx = document.getElementById("allChart");
+	var allChart = new Chart(ctx, {
+		type: 'line',
+		responsive: true,
+		maintainAspectRatio: true,
+		data: {
+			labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+			datasets: [{
+				label: 'Bugs Found',
+				data: [12, 19, 3, 78, 2, 73, 123, 53, 7, 35, 66, 4],
+				backgroundColor: 'rgba(72,126,173,.3)',
+				borderColor: 'rgba(108,108,108,1)',
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero:true
+					}
+				}]
+			}
+		}
+	});
+	
 });
 
 
@@ -49,118 +75,257 @@ angular.module('qaApp').controller('dayCtrl', function($scope) {
 	
 	$scope.test = "Reports: Today";
 	
-	// $scope.htmlEndTime = () => {
-	// 	let date = new Date();
-	// 	assessmentService.htmlEndTime($scope.user.id, date);
-	// 	$scope.stop1 = false;
-	// };
+	// $scope.date = moment().format('L');
+	
+	// var sql = cartodb.SQL({ user: 'andrew' });
+	// sql.execute("SELECT date_part('Month', t.date) as month, count(*) total, sum(damage)  damage FROM tornados t GROUP BY date_part('Month', t.date) ORDER BY date_part('Month', t.date) ASC").done(function(data) {
+	// 	console.log(data)
+	// 	var total = [];
+	// 	var damage = [];
+	// 	for (i in data.rows){
+	// 		total.push(data.rows[i].total)
+	// 		damage.push(data.rows[i].damage)
+	// 	}
+	// 	console.log(data);
+	//
+	
+	let date = new Date;
+	let day = date.getDay(); //day of the week (0-6)
+	let day2 = date.getDate(); //day of the month (1-31)
+	let month = date.getMonth(); //month of the year (0-11)
+	
+	
+	
+	
+		
+		// $scope.getDayData = () => {
+		// 	dayService.getDayData().then(function(res, err){
+		// 		console.log("homeCtrl", res, err);
+		// 		$scope.dayCount = res.data[0].totalcount;
+		// 	});
+		// };
+		// getTotalCount();
+	
+		
+		
+	var ctx = document.getElementById("dayChart");
+	var dayChart = new Chart(ctx, {
+		type: 'line',
+		responsive: true,
+		maintainAspectRatio: true,
+		data: {
+			labels: ["01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"],
+			datasets: [{
+				label: 'Bugs Found',
+				data: [2, 159, 343, 486, 274, 433, 413, 553, 447, 355, 646, 443, 244, 159, 343, 386, 524, 733, 413, 553, 447, 355, 646, 43],
+				backgroundColor: 'rgba(72,126,173,.3)',
+				borderColor: 'rgba(108,108,108,1)',
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero:true
+					}
+				}]
+			}
+		}
+	});
 	
 });
 
 
-angular.module('qaApp').controller('monthCtrl', function($scope) {
+// angular.module('qaApp').service('homeService', function($http, $q) {
+//
+//
+// 	// this.getDayData = function() {
+// 	// 	return $http ({
+// 	// 		method: 'GET',
+// 	// 		url: '/getDayData'
+// 	// 	})
+// 	// };
+// 	//
+//
+// });
+angular.module('qaApp').controller('homeCtrl', function($scope, $rootScope, homeService) {
 	
-	$scope.test = "Reports: This Month";
+	const max = 99999;
+	const min = 0;
+	const date = new Date;
+	const day = date.getDate(); //day of the month (1-31)
+	const month = date.getMonth(); //month of the year (0-11)
+	const hour = date.getHours(); //Hour time (0-23)
+	const year = date.getFullYear(); //4 digit year
 	
-	// $scope.htmlEndTime = () => {
-	// 	let date = new Date();
-	// 	assessmentService.htmlEndTime($scope.user.id, date);
-	// 	$scope.stop1 = false;
-	// };
 	
-});
-
-
-
-
-angular.module('qaApp').controller('homeCtrl', function($scope, homeService) {
+	let getTodayCount = (day, month, year) => {
+		homeService.getTodayCount(day, month, year).then(function(res, err){
+			// console.log(res.data);
+			$scope.todayCount = res.data[0].count;
+		});
+	};
+	getTodayCount(day, month, year);
 	
-	$scope.totalCount = 0;
-	$scope.todayCount = 0;
+	let getTotalCount = () => {
+		homeService.getTotalCount().then(function(res, err){
+			$rootScope.totalCount = res.data[0].count;
+			console.log(res.data[0].count);
+		});
+	};
+	getTotalCount(day, month, year, hour);
 	
 	$scope.increment = () => {
-		const date = new Date();
+		const date = new Date;
+		const day = date.getDate(); //day of the month (1-31)
+		const month = date.getMonth(); //month of the year (0-11)
+		const hour = date.getHours(); //Hour time (0-23)
+		const year = date.getFullYear(); //4 digit year
+		if ($scope.todayCount >= max) {
+			return;
+		}
 		$scope.todayCount++;
 		$scope.totalCount++;
-		homeService.increment($scope.todayCount, $scope.totalCount, date);
+		homeService.increment($scope.todayCount, $scope.totalCount, day, month, year, hour);
 	};
 	
 	$scope.decrement = () => {
-		const date = new Date();
+		if ($scope.todayCount <= min) {
+			return;
+		}
 		$scope.todayCount--;
 		$scope.totalCount--;
-		homeService.decrement($scope.todayCount, $scope.totalCount, date);
+		homeService.decrement();
 	};
 	
+	//Popup Information Modal
+	const modal = document.getElementById('popup');
+	const info = document.getElementById("info");
+	const span = document.getElementsByClassName("close")[0];
 	
-	
-	let getTodayCount = () => {
-		homeService.getTodayCount().then(function(res){
-			$scope.todayCount = res.data;
-		});
+	info.onclick = function() {
+		modal.style.display = "block";
 	};
-	getTodayCount();
 
-	let getTotalCount = () => {
-		homeService.getTotalCount().then(function(res){
-			$scope.totalCount = res.data;
-		});
+	span.onclick = function() {
+		modal.style.display = "none";
 	};
-	getTotalCount();
 
+	window.onclick = function(event) {
+		if (event.target === popup) {
+			modal.style.display = "none";
+		}
+	}
 	
 });
 angular.module('qaApp').service('homeService', function($http, $q) {
-
-	this.increment = (today, total, date) => {
-		console.log(today, total, date);
-		return $http({
-			method: 'POST',
-			url: '/incrementCount',
-			data: {
-				'today' : today,
-				'total' : total,
-				'date' : date
-			}
-		})
-	};
-
-	this.decrement = (today, total, date) => {
-		return $http({
-			method: 'POST',
-			url: '/decrementCount',
-			data: {
-				'today' : today,
-				'total' : total,
-				'date' : date
-			}
-		})
-	};
 	
-	this.getTodayCount = function() {
+	this.getTodayCount = function(day, month, year) {
 		return $http ({
 			method: 'GET',
-			url: '/getTodayCount'
+			url: '/getTodayCount',
+			params: {
+				'day': day,
+				'month' : month,
+				'year' : year
+			}
 		})
 	};
-	
+
 	this.getTotalCount = function() {
 		return $http ({
 			method: 'GET',
 			url: '/getTotalCount'
 		})
 	};
+	
+	this.increment = (today, total, day, month, year, hour) => {
+		return $http({
+			method: 'POST',
+			url: '/incrementCount',
+			data: {
+				'today' : today,
+				'total' : total,
+				'day' : day,
+				'month' : month,
+				'year' : year,
+				'hour' : hour
+			}
+		})
+	};
+	
+	this.decrement = () => {
+		return $http({
+			method: 'DELETE',
+			url: '/decrementCount',
+		})
+	};
 
 });
+angular.module('qaApp').controller('monthCtrl', function($scope) {
+	
+	$scope.test = "Reports: This Month";
+	
+	var ctx = document.getElementById("monthChart");
+	var monthChart = new Chart(ctx, {
+		type: 'line',
+		responsive: true,
+		maintainAspectRatio: true,
+		data: {
+			labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10","11", "12", "13", "14", "15", "16", "17", "18", "19", "20","21", "22", "23", "24", "25", "26", "27", "28", "29", "30","31"],
+			datasets: [{
+				label: 'Bugs Found',
+				data: [32, 659, 343, 386, 274, 533, 213, 353, 27, 345, 346, 33, 324, 659, 343, 386, 274, 733, 213, 353, 27, 345, 246, 33, 32, 659, 343, 486, 274, 533, 213],
+				backgroundColor: 'rgba(72,126,173,.3)',
+				borderColor: 'rgba(108,108,108,1)',
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero:true
+					}
+				}]
+			}
+		}
+	});
+	
+});
+
+
+
 angular.module('qaApp').controller('weekCtrl', function($scope) {
 	
 	$scope.test = "Reports: This Week";
 	
-	// $scope.htmlEndTime = () => {
-	// 	let date = new Date();
-	// 	assessmentService.htmlEndTime($scope.user.id, date);
-	// 	$scope.stop1 = false;
-	// };
+	var ctx = document.getElementById("weekChart");
+	var weekChart = new Chart(ctx, {
+		type: 'line',
+		responsive: true,
+		maintainAspectRatio: true,
+		data: {
+			labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+			datasets: [{
+				label: 'Bugs Found',
+				data: [222, 859, 743, 786, 44, 533, 473],
+				backgroundColor: 'rgba(72,126,173,.3)',
+				borderColor: 'rgba(108,108,108,1)',
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero:true
+					}
+				}]
+			}
+		}
+	});
 	
 });
 
