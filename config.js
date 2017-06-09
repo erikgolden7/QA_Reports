58a257c16d2c6c9227dde91709f1e748b92861a4
Erik Golden <erikgolden7@gmail.com>
2017-06-09 18:59:26 +0000

    fixed server

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
 	port:8083,
-=======
-	port:3000,
->>>>>>> bcdfa49f7e65325b87b51f6be21a6b0bbc9f58c8
 	postgresUser:'ypljdmop',
 	postgresPass:'1GgJ9SPiNfxxPnnLcQdzJ7Ae8TjuixRe'
 };
