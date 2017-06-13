f5f0227770dfca0f59a1ea6c2898b8690f4f7512
Erik Golden <erikgolden7@gmail.com>
2017-06-13 11:39:32 -0600

    day chart data is working

--- public/states/day_reports/dayCtrl.js
+++ public/states/day_reports/dayCtrl.js
@@ -1,88 +1,64 @@
-angular.module('qaApp').controller('dayCtrl', function($scope, dayService) {
+angular.module('qaApp').controller('dayCtrl', function($scope, $http) {
 	
-	$scope.test = "Reports: Today";
+	$scope.title = "Reports: Today";
+	const hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+	const date = new Date;
+	const day = date.getDate();
 	
-	// var one = [];
-	
-	let day = () => {
-		const date = new Date;
-		const day = date.getDate(); //day of the month (1-31)
-		dayService.getDayData(day).then((res, err) => {
-			console.log(res);
-			// sortHours(res.data, day);
-			$scope.dayData = res;
-			// for (key in res.data) {
-			// 	console.log(res.data[key].day, day);
-			// 	if (res.data[key].day === day) {
-			// 		one.push(res.data[key].day);
-			// 	}
-			// }
-
-			// console.log(one);
-			return one;
-		})
-	};
-	day();
-	
-	
-	// let sortHours = (data, day) => {
-	// 	for (key in data) {
-	// 		console.log(data[key].day, day);
-	// 		if (data[key].day === day) {
-	// 			one.push(data[key].day);
-	// 		}
-	// 	}
-	// 	return one;
-	// 	// for(let i = 0; i < data.length; i++) {
-	// 	// 	if (data[i].hour !== 9) {
-	// 	// 		one.push(data[i].hour);
-	// 	// 	}
-	// 	// }
-	// };
-	// console.log(one);
-	var one = dayService.one;
-	
-	
-	
-	function convertToArray(obj) {
-		return Object.keys(obj).map(function(key) {
-			// console.log(obj[key]);
-			return obj[key];
+	function createChart(day) {
+		$http ({
+			method: 'GET',
+			url: '/getDayData',
+			params: {
+				'day': day
+			}
+		}).then(function successCallback(res) {
+			console.log("success");
+			var max = 24;
+			var arr = [];
+			for (let i = 0; i < max; i++) {
+				var count = 0;
+				for (key in res.data) {
+					if (res.data[key].hour === i) {
+						count++;
+					}
+				}
+				arr.push(count);
+			}
+			var ctx = document.getElementById("dayChart");
+			var dayChart = new Chart(ctx, {
+				type: 'line',
+				responsive: true,
+				maintainAspectRatio: true,
+				data: {
+					labels: hours,
+					datasets: [{
+						label: 'Bugs Found',
+						data: arr,
+						backgroundColor: 'rgba(72,126,173,.3)',
+						borderColor: 'rgba(108,108,108,1)',
+						borderWidth: 1
+					}]
+				},
+				options: {
+					scales: {
+						yAxes: [{
+							ticks: {
+								beginAtZero: true
+							}
+						}]
+					}
+				}
+			})
+		}, function errorCallback(res) {
+			console.log('failed to get day data')
 		});
 	}
-	
-	
-	var xAxis = JSON.parse('{"0":"name1","2":"name2","3":"name3","4":"name4"}');
-	var yAxis = JSON.parse('{"0":"8.4113","2":"9.5231","3":"9.0655","4":"7.8400"}');
-	
-	yAxis = convertToArray(yAxis);
-	xAxis = convertToArray(xAxis);
-	
-	var ctx = document.getElementById("dayChart");
-	var dayChart = new Chart(ctx, {
-		type: 'line',
-		responsive: true,
-		maintainAspectRatio: true,
-		data: {
-			labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],
-			datasets: [{
-				label: 'Bugs Found',
-				data: day(),
-				backgroundColor: 'rgba(72,126,173,.3)',
-				borderColor: 'rgba(108,108,108,1)',
-				borderWidth: 1
-			}]
-		},
-		options: {
-			scales: {
-				yAxes: [{
-					ticks: {
-						beginAtZero:true
-					}
-				}]
-			}
-		}
-	});
-	
+	createChart(day);
 });
+
+
+
+	
+
 
--- public/states/month_reports/monthCtrl.js
+++ public/states/month_reports/monthCtrl.js
@@ -1,33 +1,88 @@
-angular.module('qaApp').controller('monthCtrl', function($scope) {
+angular.module('qaApp').controller('monthCtrl', function($scope, $http) {
 	
 	$scope.test = "Reports: This Month";
 	
-	var ctx = document.getElementById("monthChart");
-	var monthChart = new Chart(ctx, {
-		type: 'line',
-		responsive: true,
-		maintainAspectRatio: true,
-		data: {
-			labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10","11", "12", "13", "14", "15", "16", "17", "18", "19", "20","21", "22", "23", "24", "25", "26", "27", "28", "29", "30","31"],
-			datasets: [{
-				label: 'Bugs Found',
-				data: [32, 659, 343, 386, 274, 533, 213, 353, 27, 345, 346, 33, 324, 659, 343, 386, 274, 733, 213, 353, 27, 345, 246, 33, 32, 659, 343, 486, 274, 533, 213],
-				backgroundColor: 'rgba(72,126,173,.3)',
-				borderColor: 'rgba(108,108,108,1)',
-				borderWidth: 1
-			}]
-		},
-		options: {
-			scales: {
-				yAxes: [{
-					ticks: {
-						beginAtZero:true
-					}
-				}]
-			}
-		}
-	});
+// 	var ctx = document.getElementById("monthChart");
+// 	var monthChart = new Chart(ctx, {
+// 		type: 'line',
+// 		responsive: true,
+// 		maintainAspectRatio: true,
+// 		data: {
+// 			labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10","11", "12", "13", "14", "15", "16", "17", "18", "19", "20","21", "22", "23", "24", "25", "26", "27", "28", "29", "30","31"],
+// 			datasets: [{
+// 				label: 'Bugs Found',
+// 				data: [32, 659, 343, 386, 274, 533, 213, 353, 27, 345, 346, 33, 324, 659, 343, 386, 274, 733, 213, 353, 27, 345, 246, 33, 32, 659, 343, 486, 274, 533, 213],
+// 				backgroundColor: 'rgba(72,126,173,.3)',
+// 				borderColor: 'rgba(108,108,108,1)',
+// 				borderWidth: 1
+// 			}]
+// 		},
+// 		options: {
+// 			scales: {
+// 				yAxes: [{
+// 					ticks: {
+// 						beginAtZero:true
+// 					}
+// 				}]
+// 			}
+// 		}
+// 	});
+//
+// });
 	
+	$scope.title = "Reports: Today";
+	const days = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10","11", "12", "13", "14", "15", "16", "17", "18", "19", "20","21", "22", "23", "24", "25", "26", "27", "28", "29", "30","31"];
+	const date = new Date;
+	const day = date.getDate();
+	
+	function createChart(day) {
+		$http ({
+			method: 'GET',
+			url: '/getDayData',
+			params: {
+				'day': day
+			}
+		}).then(function successCallback(res) {
+			console.log("success");
+			var max = 24;
+			var arr = [];
+			for (let i = 0; i < max; i++) {
+				var count = 0;
+				for (key in res.data) {
+					if (res.data[key].hour === i) {
+						count++;
+					}
+				}
+				arr.push(count);
+			}
+			var ctx = document.getElementById("dayChart");
+			var dayChart = new Chart(ctx, {
+				type: 'line',
+				responsive: true,
+				maintainAspectRatio: true,
+				data: {
+					labels: days,
+					datasets: [{
+						label: 'Bugs Found',
+						data: arr,
+						backgroundColor: 'rgba(72,126,173,.3)',
+						borderColor: 'rgba(108,108,108,1)',
+						borderWidth: 1
+					}]
+				},
+				options: {
+					scales: {
+						yAxes: [{
+							ticks: {
+								beginAtZero: true
+							}
+						}]
+					}
+				}
+			})
+		}, function errorCallback(res) {
+			console.log('failed to get day data')
+		});
+	}
+	createChart(day);
 });
-
-
