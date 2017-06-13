f2c30a917df5e8c73d6da01b9c8f3fd6ddadd72c
Erik Golden <erikgolden7@gmail.com>
2017-06-13 16:56:49 -0600

    added bar graph to day

--- public/js/app.js
+++ public/js/app.js
@@ -13,6 +13,11 @@ angular.module('qaApp', ['ui.router']).config(function($stateProvider, $urlRoute
 		templateUrl: '../states/day_reports/day.html',
 		controller: 'dayCtrl'
 	})
+	.state('day_bar', {
+		url: '/day_bar',
+		templateUrl: '../states/day_bar/day_bar.html',
+		controller: 'dayBarCtrl'
+	})
 	.state('week', {
 		url: '/week',
 		templateUrl: '../states/week_reports/week.html',
@@ -20,12 +25,12 @@ angular.module('qaApp', ['ui.router']).config(function($stateProvider, $urlRoute
 	})
 	.state('month', {
 		url: '/month',
-		templateUrl: '../states/month_reports/month.html',
+		templateUrl: '../states/month/month_line/month.html',
 		controller: 'monthCtrl'
 	})
 	.state('month_bar', {
 		url: '/month_bar',
-		templateUrl: '../states/month_bar/month_bar.html',
+		templateUrl: '../states/month/month_bar/month_bar.html',
 		controller: 'monthBarCtrl'
 	})
 	.state('all', {
---
+++ public/states/day_bar/dayBarCtrl.js
@@ -0,0 +1,59 @@
+angular.module('qaApp').controller('dayBarCtrl', function($scope, $http) {
+	
+	$scope.title = "Reports: Today";
+	const hours = ["1am", "2am", "3am", "4am", "5am", "6am", "7am", "8am", "9am", "10am", "11am", "12am", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm", "12pm"];
+	const date = new Date;
+	const day = date.getDate();
+	
+	function createChart(day) {
+		$http ({
+			method: 'GET',
+			url: '/getDayData',
+			params: {
+				'day': day
+			}
+		}).then(function successCallback(res) {
+			console.log("day success");
+			var max = 24;
+			var arr = [];
+			for (let i = 0; i < max; i++) {
+				var count = 0;
+				for (key in res.data) {
+					if (res.data[key].hour === i) {
+						count++;
+					}
+				}
+				arr.push(count);
+			}
+			var ctx = document.getElementById("dayChart");
+			var dayChart = new Chart(ctx, {
+				type: 'bar',
+				responsive: true,
+				maintainAspectRatio: true,
+				data: {
+					labels: hours,
+					datasets: [{
+						label: 'Bugs Found',
+						data: arr,
+						backgroundColor: 'rgba(171, 214, 174,0.6)',
+						borderColor: 'rgba(108,108,108,1)',
+						borderWidth: 1
+					}]
+				},
+				options: {
+					scales: {
+						yAxes: [{
+							ticks: {
+								beginAtZero: true
+							}
+						}]
+					}
+				}
+			})
+		}, function errorCallback(res) {
+			console.log('failed to get day data')
+		});
+	}
+	createChart(day);
+});
+
